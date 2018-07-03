961f9f961c0d137305bda3c9052bfc254a8a8921
sidharth.kaliappan@gmail.com <sidharth.kaliappan@gmail.com>
2018-07-03 14:21:58 +0200

    OpenCv added with errors

---
+++ MyApplication/app/src/main/java/com/example/cui/myapplication/PreProcessing.java
@@ -0,0 +1,23 @@
+package com.example.cui.myapplication;
+
+import android.graphics.Bitmap;
+import android.util.Log;
+
+import org.opencv.android.Utils;
+import org.opencv.core.Core;
+import org.opencv.core.CvType;
+import org.opencv.core.Mat;
+import org.opencv.core.MatOfDouble;
+import org.opencv.core.MatOfPoint;
+import org.opencv.core.MatOfPoint2f;
+import org.opencv.core.Point;
+import org.opencv.core.Rect;
+import org.opencv.core.Scalar;
+import org.opencv.core.Size;
+import org.opencv.imgproc.Imgproc;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class PreProcessing {
+}
